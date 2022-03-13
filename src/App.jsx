02d31724eb5fe2a92304887f01190bf76a1d0722
MrJacooo <sebastian.storz2003@gmail.com>
2022-03-13 16:38:18 +0100

    React Router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 import { AuthProvider } from './contexts/AuthProvider';
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import { auth } from './base/firebase';
+import { Route, Routes } from 'react-router-dom';
+import WelcomePage from './components/welcomePage';
 
 function App() {
   return (
     <div className="App">
       <AuthProvider>
-        <button onClick={() => signInWithEmailAndPassword(auth, "test@test.com", "asdfasdf")} >Sign IN</button>
+        <Routes>
+          <Route path='/' element={<WelcomePage />}></Route>
+        </Routes>
       </AuthProvider>
-    </div>
+    </div >
   );
 }
 
