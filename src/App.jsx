525ea996019952a22f7058c17b1af907c79e00c2
MrJacooo <sebastian.storz2003@gmail.com>
2022-03-17 22:05:17 +0100

    Login page Style

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,44 @@ import './App.css';
 import { AuthProvider } from './contexts/AuthProvider';
 import { Route, Routes } from 'react-router-dom';
 import WelcomePage from './components/welcomePage';
+import { createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@emotion/react';
+
 
 function App() {
+
+  const theme = createTheme({
+    palette: {
+      type: 'light',
+      primary: {
+        main: '#2fd20d',
+        contrastText: '#fafafa',
+      },
+      secondary: {
+        main: '#9d00ec',
+        contrastText: '#fafafa',
+      },
+      success: {
+        main: '##00f9ba',
+      },
+      info: {
+        main: "#5685fd",
+        contrastText: '#fafafa',
+      },
+      danger: {
+        main: "#fd4239",
+        contrastText: '#fafafa',
+      }
+    }
+  })
   return (
     <div className="App">
       <AuthProvider>
-        <Routes>
-          <Route path='/' element={<WelcomePage />}></Route>
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <Routes>
+            <Route path='/' element={<WelcomePage />}></Route>
+          </Routes>
+        </ThemeProvider>
       </AuthProvider>
     </div >
   );
--- src/components/welcomePage.jsx
+++ src/components/welcomePage.jsx
@@ -1,8 +1,25 @@
+import { Button, Container, Grid, Input, TextField } from "@mui/material";
+import "../styles/welcomePage.css"
+
 
 export default function WelcomePage() {
     return (
         <>
-            Welcome
+            <Container maxWidth="lg">
+                <header>
+                    <Button variant="outlined" >Teacher Login</Button>
+                </header>
+            </Container>
+            <main>
+                <Grid container spacing={1}>
+                    <Grid item xs={12} lg={8}>
+                        <TextField className="input" label="Entry Code eingeben" variant="outlined" />
+                    </Grid>
+                    <Grid item xs={12} lg={4}>
+                        <Button className="enterButton" variant="contained" size="Large">Enter</Button>
+                    </Grid>
+                </Grid>
+            </main>
         </>
     )
 }
