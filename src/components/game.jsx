71954ee98d8344a4a90fb636c8cca688734c5ff1
MrJacooo <sebastian.storz2003@gmail.com>
2022-03-22 18:11:07 +0100

    Revert "pre Firebase"
    This reverts commit 7bef4c924c4979c68eb8f76af77aeb45b8bf1372.

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -1,14 +1,7 @@
-import { Edit, PlayArrow } from "@mui/icons-material";
-import { Button } from "@mui/material";
-
-export default function Game(props) {
-    return (
+export default function Game(){
+    return(
         <div className="gameContainer">
-            <h3>{props.title}</h3>
-            <div>
-                <Button variant="contained" className="iconButton" color="info"><Edit /></Button>
-                <Button variant="contained" className="iconButton" ><PlayArrow /></Button>
-            </div>
+            asdf
         </div>
     )
 }
