ee05137b28fee65c749f0a8bcc16a0de589a240b
MrJacooo <sebastian.storz2003@gmail.com>
2022-03-22 19:08:24 +0100

    Dashboard Style

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,29 +1,30 @@
-import { Dashboard as Dashboardicon, Person } from "@mui/icons-material";
-import { Container, Paper, Tab, Tabs, Box } from "@mui/material";
-import { useState } from "react";
+import { Dashboard as Dashboardicon, Person, Public } from "@mui/icons-material";
+import { Container, Paper, Tab, Tabs, Box, Grid } from "@mui/material";
+import { useEffect, useState } from "react";
 import "../styles/dashboard.css"
 import Game from "./game";
 
-export default function Dashboard(){
-    const [tab, setTab] = useState("dashboard")
-    return(
+export default function Dashboard() {
+    const [tab, setTab] = useState("account")
+
+    useEffect(() => {
+        //fetch Games from Cloud
+    }, [])
+    return (
         <>
-            {window.innerWidth > 600?<NonMobileLayout tab={tab} setTab={setTab}/>:"mobile"}
+            <Container maxWidth="lg">
+                <Tabs value={tab} onChange={(e, newVal) => setTab(newVal)} >
+                    <Tab value="account" label="Account" icon={<Person />} />
+                    <Tab value="public" label="Public" icon={<Public />} disabled />
+                </Tabs>
+            </Container>
             <Container className="colorContainer" maxWidth="lg">
-                <Game />
+                <Grid container spacing={2}>
+                    <Grid item xs="12" sm="6" lg="4">
+                        <Game title="English Game 1" />
+                    </Grid>
+                </Grid>
             </Container>
         </>
-    )
-}
-
-function NonMobileLayout(props){
-    return(
-        <Container maxWidth="lg">
-            <Tabs value={props.tab} onChange={(e, newVal) => props.setTab(newVal)}>
-                <Tab value="dashboard" label="Dashboard" icon={<Dashboardicon />}/>
-                <Tab value="account"  label="Account" icon={<Person />}/>
-            </Tabs>
-            </Container>
-        
     )
 }
